b0a1ff3642d27935b7e38e8494b29f853ca62025
Ilya <kalashilya@yandex.ru>
2022-06-13 10:47:14 +0300

    db_test

--- src/main/java/test_pg_admin/PgAdminApp.java
+++ src/main/java/test_pg_admin/PgAdminApp.java
@@ -3,17 +3,25 @@ package test_pg_admin;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PgAdminApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         getDriver();
+        Connection connection = getConnection();
+        connection.setAutoCommit(false);
+        Statement statement = connection.createStatement();
         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=12345");
+        int result = statement.executeUpdate("INSERT INTO test VALUES ('environment', 753)");
+            System.out.println("Insert success");
+            System.out.println(result + "lines inserted");
+            connection.commit();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            connection.rollback();
         }
-
+        connection.close();
     }
+
     private static void getDriver() {
         try {
             Class.forName("org.postgresql.Driver");
@@ -22,6 +30,17 @@ public class PgAdminApp {
             System.out.println("Failed");
             throw new RuntimeException(e);
         }
+    }
+
+    private static Connection getConnection() {
+        Connection connection;
+        String dbUrl = System.getenv("DB_URL") + System.getenv("DB_USER&PASS");
+        try {
+            connection = DriverManager.getConnection(dbUrl);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
     }
 }
 
